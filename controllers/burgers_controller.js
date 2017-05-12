1c036afb88936c394f9ac6625d2593f64da80ec5
Jieun Rim <rim.jieun@gmail.com>
2017-05-12 15:17:43 -0400

    updated burgers_controller.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  burger.create("burger_name", req.body.burger_name, function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update(
+  {
+    devoured: req.body.devoured
+  },
+  condition, function() {
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
